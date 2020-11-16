ff5b2e808e3bd9041547bd0fa8a9e969f19481b2
IcyEzra <uzairbaig.work@gmail.com>
2020-11-16 10:39:03 -0500

    added nice comment about b21

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,9 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("Hello git last day!");
+        //B21 is the best
+
+
+
     }
 }
